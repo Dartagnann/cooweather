1dad1520b7e2689168a1c5bb8f463f48226d1081
Oliver <374276419@qq.com>
2018-01-11 22:49:12 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 123 on 2018/1/11.
+ */
+
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,8 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by 123 on 2018/1/11.
+ */
+
+public class Utility {
+}
